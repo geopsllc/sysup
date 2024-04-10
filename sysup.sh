5c068cb9e05134e734074723fdb9a46c0712e9d6
Georgi Stoyanov <33185290+geopsllc@users.noreply.github.com>
2024-04-10 13:46:10 -0500

    0.1

--- sysup.sh
+++ sysup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 als="$(cat $HOME/.bashrc | grep sysup)"
-nr="$(dpkg -l needrestart | grep 'no description available')"
+nr="$(dpkg -l needrestart 2>/dev/null | grep daemons)"
 
 if [ -z "$als" ]; then
   echo "alias sysup=$PWD/sysup.sh" >> $HOME/.bashrc
 fi
 
-if [ -z "$nr" ]; then
+if [ ! -z "$nr" ]; then
   sudo apt-get purge needrestart -y
   echo $nr
 fi
